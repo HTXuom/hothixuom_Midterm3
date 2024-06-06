6eb9c5331d2c1dd901da444dbb17041d0360a8c3
HTXuom <xuom.ho25@student.passerellesnumeriques.org>
2024-06-06 21:43:06 +0700

    commit home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Navbar from './components/layout/Navbar';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.github.com/users");
+        console.log("GitHub Users:", response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
-      <div className="Container">
-       <h1> Hello from React </h1>
+      <div className="container">
+        <h1>Hello from React</h1>
       </div>
     </div>
   );
