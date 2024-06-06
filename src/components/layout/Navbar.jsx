44b8e8cab4eee66ac26cffcad2d67ed64056f06c
HTXuom <xuom.ho25@student.passerellesnumeriques.org>
2024-06-06 20:40:51 +0700

    commit Navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-success">
+      <h1>
+        <i className="fab fa-github" /> GitHub Finder
+      </h1>
+      <ul>
+        <li>
+          <a href="#>">Home</a>
+          <a href="#>">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
