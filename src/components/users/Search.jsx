bde4903a5b4de4fe8491266c296020169f709933
HTXuom <xuom.ho25@student.passerellesnumeriques.org>
2024-06-07 11:14:38 +0700

    commit User

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,22 +1,20 @@
 import axios from "axios";
-import React, { useState } from "react";
+import { useState } from "react";
 import Users from "./Users";
-
 const Search = () => {
     const [text, setText] = useState("");
     const [users, setUsers] = useState([]);
-
     const searchUsers = async (text) => {
         try {
-            const response = await axios.get(
-                `https://api.github.com/search/users?q=${text}`
-            );
+            const response = await axios.get(`https://api.github.com/search/users?q=${text} `);
             setUsers(response.data.items);
         } catch (error) {
             console.error("Error fetching data:", error);
         }
     };
-
+    const clearUsers = () => {
+        setUsers([]);
+    };
     const onSubmit = (e) => {
         e.preventDefault();
         if (text === "") {
@@ -26,28 +24,18 @@ const Search = () => {
             setText("");
         }
     };
-
     const onChange = (e) => setText(e.target.value);
-
     return (
         <div>
             <form onSubmit={onSubmit} className="form">
-                <input
-                    type="text"
-                    name="text"
-                    placeholder="Search User"
-                    value={text}
-                    onChange={onChange}
-                />
-                <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-success btn-block"
-                />
+                <input type="text" name="text" placeholder="Search User" value={text} onChange={onChange} />
+                <input type="submit" value="Search" className="btn btn-success btn-block" />
             </form>
+            <button className="btn btn-danger btn-block" onClick={clearUsers}>
+                Clear
+            </button>
             <Users users={users} />
         </div>
     );
 };
-
 export default Search;
